e23e8f6c41e0b8df3547b40161b420b820a50556
divinedavis <divinejdavis@gmail.com>
2015-08-07 11:22:53 -0400

    First commit Adding tableview Adding cell information

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Divine Davis on 8/7/15.
+//  Copyright (c) 2015 Juniper Street Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel!.text = "Hello"
+        return cell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
