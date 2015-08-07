3a41579fa074679c5cf1cd8b274f1e93f962bd01
divinedavis <divinejdavis@gmail.com>
2015-08-06 22:04:16 -0400

    First commit

---
+++ Practice With Swift 1/Practice With Swift 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice With Swift 1
+//
+//  Created by Divine Davis on 8/6/15.
+//  Copyright (c) 2015 Juniper Street Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
