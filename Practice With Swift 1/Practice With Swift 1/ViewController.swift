2f8fb220a0898a9bbfc9bf76597017c955a832aa
divinedavis <divinejdavis@gmail.com>
2015-08-06 22:15:05 -0400

    Created a date picker with alert

--- Practice With Swift 1/Practice With Swift 1/ViewController.swift
+++ Practice With Swift 1/Practice With Swift 1/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func displayDay(sender: AnyObject) {
+        
+        //Grab the selected day from the date picker
+        var chosenDate = self.datePicker.date
+        
+        //Create date formatter
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        
+        //Grab the day and create a message
+        let day = formatter.stringFromDate(chosenDate)
+        let result = "That was a \(day)"
+        
+        //Create alert
+        let myAlert = UIAlertController(title: result, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        //Show alert
+        self.presentViewController(myAlert, animated: true, completion: nil)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
