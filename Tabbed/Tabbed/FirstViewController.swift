34f7b7b70f771f7077993d57370c4e1c4ba89dae
divinedavis <divinejdavis@gmail.com>
2015-08-10 12:26:59 -0400

    Doing a tabbed application

---
+++ Tabbed/Tabbed/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed
+//
+//  Created by Divine Davis on 8/10/15.
+//  Copyright (c) 2015 Juniper Street Games. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tabbed/Tabbed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed
+//
+//  Created by Divine Davis on 8/10/15.
+//  Copyright (c) 2015 Juniper Street Games. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
