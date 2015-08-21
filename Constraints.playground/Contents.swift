8ac19f9cca300d10e95f4c1ec32e5379920d35e5
divinedavis <divinejdavis@gmail.com>
2015-08-21 01:05:19 -0400

    First

---
+++ Constraints.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let str = "Hello playground"
+
+let firstName = "Grace"
+let interestRate : Float = 4.5
+let isActive = true
+
+
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var highScore = 100
+var firstName = "Grace"
+var isActive = true
+
+var high : Int
+
+var myFloat : Float //32-bit floating point
+var myDouble : Double //64-bit floating point
+
+var myString : String
+var myChar : Character
+
+var myBool : Bool
+
+var lastName : String = "Hopper"
+var isOptional : Bool = true
+var voltage = 240
+
+var interestRate : Float
+
