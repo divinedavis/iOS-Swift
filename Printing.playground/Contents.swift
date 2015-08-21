e7658e8fe08109826b36c22f6dfcb9189e9cb1f6
divinedavis <divinejdavis@gmail.com>
2015-08-21 10:04:30 -0400

    Converting doubles & stuff

--- Printing.playground/Contents.swift
+++ Printing.playground/Contents.swift
@@ -10,3 +10,18 @@ var myAmount = 5000
 myAmount
 
 println("Greetings, print")
+
+println(myAmount)
+
+//String interpolation - we use \()
+
+let city = "Carpinteria"
+var day = "Tuesday"
+var temp = 75
+
+"the high for \(city) on \(day) is \(temp) degrees"
+
+var quantity = 44
+var unitPrice = 34
+
+println("the amount is \(quantity * unitPrice)")
