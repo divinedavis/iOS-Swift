a1424a18db5d661e95bdc7d7c069043e6fa370b1
divinedavis <divinejdavis@gmail.com>
2015-08-21 01:06:33 -0400

    First

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var highScore = 100
+highScore = highScore + 50
+
+for i in 0..<100 {
+    highScore = highScore + i
+}
+
