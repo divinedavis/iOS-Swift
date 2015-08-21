10516adad26bdc6350f5c9c35f8a0b5105eeae47
divinedavis <divinejdavis@gmail.com>
2015-08-21 10:08:51 -0400

    Added code

--- Converting.playground/Contents.swift
+++ Converting.playground/Contents.swift
@@ -2,4 +2,9 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var quantity = 42
+var unitPrice = 34.44
+
+println("The amount is \(Double(quantity) * unitPrice)")
+
+var result = Double(quantity) * unitPrice
