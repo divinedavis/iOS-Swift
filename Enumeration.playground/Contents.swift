27c3f43e1531a12a6cd73f70f7e264c66bc6697a
divinedavis <divinejdavis@gmail.com>
2015-08-24 20:02:03 -0400

    Enumeration

---
+++ Enumeration.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum SeatPreference {
+    case Window
+    case Middle
+    case Aisle
+    case NoPreference
+    case Back, Front
+}
+
+var jenPrefers : SeatPreference
+jenPrefers = .Aisle
+
+var royPrefers = SeatPreference.Window
+
+switch royPrefers {
+case .Window:
+    println("He preferes the WINDOW seat")
+case .Middle:
+    println("He like the MIDDLE seat")
+}
+
+
