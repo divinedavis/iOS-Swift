b81a55d0d33b3853be7c58493af02ca111b4ba55
divinedavis <divinejdavis@gmail.com>
2015-08-24 20:29:59 -0400

    Defining classes

---
+++ DefiningClasses.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Player {
+    //properties
+    var name : String?
+    var score : Int?
+    
+    //methods
+    func description() -> String {
+        return("Player \(name) has a score of \(score)")
+    }
+    
+}
+
+//instatiate a new Player object
+var jake = Player()
+
+//use dot syntax
+jake.name = "Jake"
+jake.score = 1000
+println(jake.description())
