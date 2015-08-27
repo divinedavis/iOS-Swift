0f8832ff37b1bc1513fd8caee4c05ea0bd2beba9
divinedavis <divinejdavis@gmail.com>
2015-08-27 11:06:19 -0400

    Any Object

--- AnyObject.playground/Contents.swift
+++ AnyObject.playground/Contents.swift
@@ -5,3 +5,9 @@ import UIKit
 var someObject : AnyObject
 
 someObject = "This is a simple message"
+
+let wordsArray = someObject.componentsSeparatedByString(" ")
+
+if someObject is String {
+    let wordsArray = someObject.componentsSeparatedByString(" ")
+}
