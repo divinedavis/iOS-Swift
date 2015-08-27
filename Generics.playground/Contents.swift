2f0ce1bb9c1aaa51e3c918afb5e248811c67a393
divinedavis <divinejdavis@gmail.com>
2015-08-27 12:00:50 -0400

    Playing with Navigation Controllers

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -10,14 +10,19 @@ let myInts = [123, 333, 444, 554, 555, 666]
 let myStings = ["Red", "green", "blue"]
 let myObjects = [SimpleClass(), SimpleClass(), SimpleClass()]
 
-func displayArray<SomeType>(theArray: [SomeType]) {
+func displayArray<SomeType>(theArray: [SomeType]) -> SomeType {
     println("Printing the array:")
     for eachItem in theArray {
         print(eachItem)
         print("  :  ")
     }
     println()
+    let finalElement : SomeType = theArray[theArray.count-1]
+    return finalElement
 }
+
+var finalInt = displayArray(myInts)
+++finalInt
 
 displayArray(myInts)
 displayArray(myStings)
