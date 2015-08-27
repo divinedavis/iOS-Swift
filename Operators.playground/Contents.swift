f28998c424e5fc17e389b932c7221ca04aac1ac0
divinedavis <divinejdavis@gmail.com>
2015-08-27 08:09:48 -0400

    Operators

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a = 5
+var b = 50
+var c = 11.38
+var highScore = 50000
+
+//arithmetic operator: + - * /
+var result = a + b
+result = b - 20
+var x = c * 3.23
+x = c / 1.1
+
+highScore = highScore + 5
+
+// compound assignment: += -=
+highScore+=50
+highScore -= 10
+
+// increment, decrement: ++ --
+//POSTfix
+var score = 99
+score++
+score--
+
+//PREfix
+--score
+++score
+    
+
