4ea417ba25b7e3c441b6ed0ccf4a4953cfb2924d
divinedavis <divinejdavis@gmail.com>
2015-08-31 11:24:25 -0400

    Manipulating strings

---
+++ Web Content/Web Content/Web Content.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var andtherStr = "Rob"
+
+var thirdStr = str + andtherStr
+
+for chars in str {
+    println(chars)
+}
+
+var newString = "Test string" as NSString
+
+var subString = newString.substringWithRange(NSRange(location: 2, length: 6))
+
+if (newString.containsString("Test")) {
+    
+    println("THERE IS!")
+    
+}
+
+newString.componentsSeparatedByString(" ")
+
+
+
