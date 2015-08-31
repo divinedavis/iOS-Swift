44265ff475ecd84b07b6ffcd69e14c1c4b4a7443
divinedavis <divinejdavis@gmail.com>
2015-08-31 11:12:11 -0400

    Learned how to get HTML data with NSURL

--- Web Content/Web Content/ViewController.swift
+++ Web Content/Web Content/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = NSURL(string: "http://stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error) in
+            
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
