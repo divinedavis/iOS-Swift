12e70846d2f54538b062890aafca24762b61417e
divinedavis <divinejdavis@gmail.com>
2015-08-31 18:15:00 -0400

    Using keywords to get the weather data

--- What's The Weather/What's The Weather/ViewController.swift
+++ What's The Weather/What's The Weather/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var urlString = "http://www.weather-forecast.com/locations" + city.text + "/forecasts/latest"
+        
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
