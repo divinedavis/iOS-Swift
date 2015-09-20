b635aef871339759e3fd76012e6998cf1fb641b9
divinedavis <divinejdavis@gmail.com>
2015-09-19 20:58:09 -0400

    Dove deeper into optionals

---
+++ OptionalsPT2.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var translatedWord : String? = "Cat"
+
+if translatedWord != nil {
+    
+    print(translatedWord!)
+}
+
