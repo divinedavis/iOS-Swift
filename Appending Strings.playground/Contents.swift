7b7d19362ad56770b652bcae043117288edfcc17
divinedavis <divinejdavis@gmail.com>
2015-09-19 20:06:44 -0400

    Learned some appending of the strings

---
+++ Appending Strings.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var beach = "Beach"
+
+beach = beach + " ball"
+
+let firstName = "Steve"
+var lastName = "Nemo"
+
+var fullName = firstName + " " + lastName
+
