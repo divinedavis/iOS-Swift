64d497f59bddfaf4e7fefc5f8ee3b54413ce2be8
divinedavis <divinejdavis@gmail.com>
2015-09-25 22:43:37 -0400

    Minor Changes

---
+++ Big O Notation.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//array of sorted integer
+var numberList : Array<Int> = [8, 2, 10, 9, 11, 7, 3, 4]
+
+func insertionSort(var numberList: Array<Int>) -> Array<Int> {
+    
+    var y, key : Int
+    for x in 0..<numberList.count {
+        
+        //obtain a key to be evaluated 
+        key = numberList[x]
+        
+        //iterate backwards through the sorted portion
+        for (y = x; y > -1; y--) {
+            
+            if (key < numberList[y]) {
+                
+                //remove item from original position
+                numberList.removeAtIndex(y + 1)
+                
+                //insert the number at the key position
+                numberList.insert(key, atIndex: y)
+            }
+        }
+    }
+    return numberList
+}
