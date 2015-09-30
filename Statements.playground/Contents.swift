b556b18b54d58370d06daddb3b0d31dc9359a397
divinedavis <divinejdavis@gmail.com>
2015-09-29 20:36:05 -0400

    Practicing more Swift. On my second book out of 4.

---
+++ Statements.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+print("hello ")
+print("world")
+
+//or
+
+print("Hello "); print("World")
+
+class Dog {
+    func bark() {
+        print("WOOF")
+    }
+}
+
+let dog = Dog()
+
+dog.bark()
+
+let sum = 1 + 2
+
+extension Int {
+    func sayHello() {
+        print("Hell, i'm \(self)")
+    }
+}
+
+1.sayHello()
