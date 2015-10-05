10c8853222d8777cb2e24e6f300dbba7250337ef
divinedavis <divinejdavis@gmail.com>
2015-10-04 20:55:20 -0400

    More function practice

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -2,49 +2,8 @@
 
 import UIKit
 
-func sum(x : Int, y : Int) -> Int {
-    
-    let result = x + y
-    
-    return result
-}
-
-sum(8, y: 0)
-
-func say1(s : String) -> Void {
-    print(s)
-}
-
-say1("What's up?")
-
-func say2(s : String) -> () {
-    print(s)
-}
-
-func say3(s : String) {
-    print(s)
-}
-
-let pointless : Void = say1("Howdi")
-
-func greet1(_ :Void) -> String {return "Howdy"}
-func greet2() -> () {print("Howdy")}
-func greet3() {print("What's up")}
-
-func repeatString(s : String, times n : Int) -> String {
-    var result = ""
-    for _ in 1...n { result += s}
-    return result
-}
-
-let s = repeatString("whats up?", times: 3)
-
-let l = "hello"
-let l2 = l.stringByReplacingOccurrencesOfString("ell", withString: "hooooo")
-l2
-
 class Dog {
-    func say(s : String, _ times : Int) {
+    func say(s : String, _ times : Int = 1) {
         for _ in 1...times {
             print(s)
         }
@@ -52,26 +11,24 @@ class Dog {
 }
 
 let d = Dog()
-d.say("woof", 3)
+d.say("Wofo", 99)
 
-func say(what : String) {
-    
+let f = Dog()
+f.say("Hi")
+
+func sayString(arrayOfStrings : String ...) {
+    for s in arrayOfStrings {
+        print(s)
+    }
 }
 
-func say(what : Int) {
-    
+sayString("What", "Do", "You", "Mean")
+
+func say(s : String, times : Int, var loudly : Bool) {
+
+    loudly = true
+    loudly = false
 }
-
-say(8)
-say("Hahhhhh")
-
-
-
-
-
-
-
-
 
 
 
