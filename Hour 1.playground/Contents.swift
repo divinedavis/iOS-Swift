f68046ce748573775176d513b15ed0564c2c1623
divinedavis <divinejdavis@gmail.com>
2015-10-11 22:28:46 -0400

    Manipulating strings

---
+++ Hour 1.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let firstString = "Whats up?"
+let secondString = " Homie"
+let bothStrings = firstString + secondString
+let uppercase = bothStrings.uppercaseString
+
+
