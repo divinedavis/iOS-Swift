07802e9f0481a2b5d77bd502d891f9c59491c1ef
divinedavis <divinejdavis@gmail.com>
2015-10-12 11:22:22 -0400

    Just learned that in Swift, most of the data types discussed are actually structs

---
+++ Hour 2.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let intMax : Int = Int.max
+
+let uint8Max = UInt8.max
+let uint8Mine = UInt8.min
+
+let int8Max = Int8.max
+let int8Min = Int8.min
+
+let uint16Max = UInt16.max
+let uint32Max = UInt32.max
+let uint64Max : UInt64 = UInt64.max
+
+
+let myInt : Int = 8
