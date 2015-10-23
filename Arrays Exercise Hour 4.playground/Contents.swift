fee07fa8b84ee662d237519d658d3665bbbb05e4
divinedavis <divinejdavis@gmail.com>
2015-10-23 17:40:53 -0400

    Manipulating arrays and dictionaries. Going through hour 4 in the book. These are becoming very comfortable with me.

---
+++ Arrays Exercise Hour 4.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myName = ["Divine", "Davis"]
+
+var myName2 = myName
+
+myName.append("PHD")
+myName.insert("Jordan", atIndex: 1)
+myName.removeLast()
+myName.removeAtIndex(1)
+
