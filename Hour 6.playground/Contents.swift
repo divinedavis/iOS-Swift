22f358cf21bb73c8b6961890ab5fd9c3da197e33
divinedavis <divinejdavis@gmail.com>
2015-10-27 10:35:25 -0400

    Simple if statements

---
+++ Hour 6.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let myColor = "blue"
+
+if myColor == "red" {
+    print("this is the color \(myColor)")
+} else if myColor == "blue" {
+    print("the sky is \(myColor)")
+} else {
+    print("the color is i dont know")
+}
+
