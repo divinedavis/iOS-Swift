768b0ef88dcfa9f8b3e3f3507069c0633f4a99db
divinedavis <divinejdavis@gmail.com>
2015-10-27 09:57:00 -0400

    Done with hour 5 in optionals - Optional binding - Implicit optionals - Explicit optionals

--- Implicit.playground/Contents.swift
+++ Implicit.playground/Contents.swift
@@ -2,11 +2,71 @@
 
 import UIKit
 
-var implicitInt = Int!()
+let odometerField = UITextField()
 
-var optionalInt : Int!
-var myDictionary = ["life" : 32]
+let lastOdometerField = UITextField()
 
-optionalInt = myDictionary.updateValue(42, forKey: "life")
+let gallonsField = UITextField()
 
-print(optionalInt)
+let avgMPGLabel = UITextField()
+
+let distanceLabel = UITextField()
+
+odometerField.text = "344344"
+
+lastOdometerField.text = "344321"
+
+gallonsField.text = "15"
+
+var odometerString = odometerField.text
+
+var lastOdometerString = lastOdometerField.text
+
+var gallonsString = gallonsField.text
+
+var avgMPGDouble = 0.0
+
+var distanceDouble = 0.0
+
+var gallonsDouble = 0.0
+
+if let odometerInt = Int(odometerString!) {
+    
+    distanceDouble = Double(odometerInt)
+}
+
+if let lastOdometerInt = Int(lastOdometerString!) {
+    
+    distanceDouble -= Double(lastOdometerInt)
+}
+
+if let gallonsInt = Int(gallonsString!) {
+    
+    gallonsDouble = Double(gallonsInt)
+    
+    avgMPGDouble = distanceDouble / gallonsDouble
+}
+
+avgMPGLabel.text = "\(avgMPGDouble) MPG"
+
+distanceLabel.text = "\(distanceDouble) miles per fill-up"
+
+avgMPGDouble
+distanceDouble
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
