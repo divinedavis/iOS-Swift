8b12df6a2385cf1171fa8be4227c80f24a28c462
divinedavis <divinejdavis@gmail.com>
2015-10-27 09:59:56 -0400

    Practicing using optional binding

---
+++ Exercise Hour 5.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var userFeedback : String?
+
+userFeedback = "Hi"
+userFeedback!
+
+if let feedback = userFeedback {
+    print("this is something \(feedback)")
+}
