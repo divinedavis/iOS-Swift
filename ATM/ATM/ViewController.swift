147740c24cfedf400a21b5fe70fd9994125163b1
divinedavis <divinejdavis@gmail.com>
2015-10-28 12:53:41 -0400

    Made an ATM iOS app

---
+++ ATM/ATM/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ATM
+//
+//  Created by Divine Davis on 10/28/15.
+//  Copyright © 2015 Divine Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBAction func withdrawButton(sender: AnyObject) {
+        
+        detailLabel.text = ""
+        
+        let withdrawn = Int(withdrawAmount.text!)!
+        let amountLeft = 200 - withdrawn
+    
+        if withdrawn > 200 {
+            detailLabel.text = "You don't have enough funds"
+            moneyLeftLabel.text = "200"
+        } else {
+            moneyLeftLabel.text = String(amountLeft)
+        }
+    }
+    
+
+    @IBAction func depositButton(sender: AnyObject) {
+        
+        detailLabel.text = ""
+        
+        let deposit = Int(withdrawAmount.text!)!
+        let addedAmount = deposit + 200
+        moneyLeftLabel.text = String(addedAmount)
+        
+    }
+    @IBOutlet weak var withdrawAmount: UITextField!
+    @IBOutlet weak var moneyLeftLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+
+
+}
+
