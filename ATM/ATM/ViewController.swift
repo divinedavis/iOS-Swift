3098f2d3f3afd159cd622a4a87661a8fe0f7c2a5
divinedavis <divinejdavis@gmail.com>
2015-10-29 14:10:52 -0400

    Creating a transfer button

--- ATM/ATM/ViewController.swift
+++ ATM/ATM/ViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var withdrawAmount: UITextField!
+    @IBOutlet weak var moneyLeftLabel: UILabel!
+    
+    @IBAction func transferButton(sender: AnyObject) {
+        
+        
+        
+    }
     @IBAction func withdrawButton(sender: AnyObject) {
         
         detailLabel.text = ""
         
         let withdrawn = Int(withdrawAmount.text!)!
+        
         let amountLeft = 200 - withdrawn
     
         if withdrawn > 200 {
+            
             detailLabel.text = "You don't have enough funds"
+            
             moneyLeftLabel.text = "200"
         } else {
             moneyLeftLabel.text = String(amountLeft)
@@ -36,8 +47,7 @@ class ViewController: UIViewController {
         moneyLeftLabel.text = String(addedAmount)
         
     }
-    @IBOutlet weak var withdrawAmount: UITextField!
-    @IBOutlet weak var moneyLeftLabel: UILabel!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
