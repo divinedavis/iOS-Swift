ce2e55a82251f473a0bce1fea931480ce28e8cfc
divinedavis <divinejdavis@gmail.com>
2015-11-19 17:08:25 -0500

    Login

---
+++ Mini Moodle #5/Mini Moodle #5/CoursesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CoursesViewController.swift
+//  Mini Moodle #5
+//
+//  Created by Divine Davis on 11/19/15.
+//  Copyright © 2015 Divine. All rights reserved.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+        
+    @IBAction func cancelToCoursesViewController(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func saveCourse(segue:UIStoryboardSegue) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mini Moodle #5/Mini Moodle #5/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Mini Moodle #5
+//
+//  Created by Divine Davis on 11/19/15.
+//  Copyright © 2015 Divine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelToLogInViewController(segue:UIStoryboardSegue) {
+    }
+
+
+
+}
+
