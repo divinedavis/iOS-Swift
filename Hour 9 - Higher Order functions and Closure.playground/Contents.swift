eb9f3d335d45a714d25941066314f225cae05bfe
divinedavis <divinejdavis@gmail.com>
2015-11-23 11:39:00 -0500

    Messing with closures

--- Hour 9 - Higher Order functions and Closure.playground/Contents.swift
+++ Hour 9 - Higher Order functions and Closure.playground/Contents.swift
@@ -79,9 +79,63 @@ func mapEachElement(inArray arr : [Int], withFunc aFunc : (Int) -> Int) -> [Int]
 let firstArray = [1,2,3,4,5]
 let squareArray = mapEachElement(inArray: firstArray, withFunc: square)
 
-func mean2(numbers : [Int]) -> Double {
-    sum = numbers.reduce(<#T##initial: T##T#>, combine: <#T##(T, Int) throws -> T#>)
+func mean2(numbers :[Int]) -> Double {
+    var sum = 0
+    func meanCombine(num1 : Int, num2 : Int) -> Int {
+        return num1 + num2
+    }
+    sum = numbers.reduce(0, combine: meanCombine)
+    return Double(sum)/Double(numbers.count)
 }
+
+mean2([5,6,7])
+
+func mean3(numbers : [Int]) -> Double {
+    let sum = numbers.reduce(0, combine: { (num1 : Int, num2 : Int) -> Int in
+        return num1 + num2 })
+    return Double(sum)/Double(numbers.count)
+}
+mean3([2,3,4,5,6,7])
+
+func mean4(numbers : [Int]) -> Double {
+    let sum = numbers.reduce(0, combine: { num1, num2 in
+        return num1 + num2 })
+    return Double(sum)/Double(numbers.count)
+}
+mean4([1,2,3,4])
+
+func mean5(numbers : [Int]) -> Double {
+    let sum = numbers.reduce(0, combine: { num1, num2 in num1 + num2 })
+    return Double(sum)/Double(numbers.count)
+}
+mean5([7,7,11])
+
+func mean6(numbers : [Int]) -> Double {
+    let sum = numbers.reduce(0, combine: { $0 + $1 })
+    return Double(sum)/Double(numbers.count)
+}
+mean6([2,3,4,5])
+
+func mean7(numbers : [Int]) -> Double {
+    let sum = numbers.reduce(0) { $0 + $1 }
+    return Double(sum)/Double(numbers.count)
+}
+
+func mean8(numbers : [Int]) -> Double {
+    return Double(numbers.reduce(0) {$0 + $1})/Double(numbers.count)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
