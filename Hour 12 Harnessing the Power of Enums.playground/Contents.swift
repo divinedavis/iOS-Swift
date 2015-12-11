7cd7790ec9285340f56df4f2542d3276e4955592
divinedavis <divinejdavis@gmail.com>
2015-12-11 00:48:42 -0500

    Going through the section

--- Hour 12 Harnessing the Power of Enums.playground/Contents.swift
+++ Hour 12 Harnessing the Power of Enums.playground/Contents.swift
@@ -2,16 +2,16 @@
 
 import UIKit
 
-enum GradesKto5 : String {
-    case Kindergarden = "K", First = "1st"
-    case Second = "2nd", Third = "3rd"
-    case Fourth = "4th", Fifth = "5th"
-    
-}
-
-var myKid = GradesKto5.Kindergarden
-myKid.rawValue
-
+//enum GradesKto5 : String {
+//    case Kindergarden = "K", First = "1st"
+//    case Second = "2nd", Third = "3rd"
+//    case Fourth = "4th", Fifth = "5th"
+//    
+//}
+//
+//var myKid = GradesKto5.Kindergarden
+//myKid.rawValue
+//
 enum PhoneNumber {
     case TollFree(areaCode : Int, exchange : Int, ext : Int)
     case InternalExtension(Int)
@@ -19,8 +19,36 @@ enum PhoneNumber {
 
 let customerService = PhoneNumber.TollFree(areaCode: 803, exchange: 333, ext: 3434)
 
+switch customerService {
+case .TollFree(_, _, let ext):
+    print("Toll-free number, extension: \(ext)")
+case .InternalExtension(let ext):
+    print("Internal extension: \(ext)")
+}
 
 
+
+enum GradesKTo5 {
+    case Kindergarten, First, Second, Third, Fourth, Fifth
+}
+
+let studentGrade = GradesKTo5.First
+
+switch studentGrade {
+    
+case .Kindergarten:
+    print("Welcome to Kindergarten")
+case .First:
+    print("Welcome to first grade")
+case .Second:
+    print("Welcome to second grade")
+case .Third:
+    print("Welcome to third grade")
+case .Fourth:
+    print("Welcome to fourth grade")
+case .Fifth:
+    print("Welcome to fifth grade")
+}
 
 
 
