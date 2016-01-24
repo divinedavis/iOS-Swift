a1fa0a2e2547952bb9cfb2aa7b255a8355e14557
divinedavis <divinejdavis@gmail.com>
2016-01-24 10:50:02 -0500

    Done with the chapter

--- Hour 17 Using Protocols to Define Behavior.playground/Contents.swift
+++ Hour 17 Using Protocols to Define Behavior.playground/Contents.swift
@@ -155,7 +155,7 @@ protocol Healable {
 
 struct BluePotion : Healable {
     var healableName : String { return "blue potion" }
-    var hearsRestores : Double { return 4.0 }
+    var heartsRestores : Double { return 4.0 }
 }
 
 struct Heart : Healable {
@@ -174,10 +174,62 @@ protocol GameManagerDelegate {
     
 }
 
+class TheLegendOfZelda {
+    let maxHeartContainers = 20
+    let minHearts = 0.0
+    let delegate : GameManagerDelegate
+    let name = "Link"
+    var sword : String?
+    var heartContainers = 3.0
+    var hearts : Double = 0.0 {
+        didSet {
+            if hearts > heartContainers {
+                hearts < heartContainers
+            }
+            if hearts <= minHearts {
+                hearts = minHearts
+            }
+        }
+    }
+    init(delegate : GameManagerDelegate) {
+        self.delegate = delegate
+        self.delegate.gameDidStart(self)
+    }
+    func takeDamage(fromEnemy enemy : Enemy) {
+        hearts -= enemy.damage
+        print("\(name) lost \(enemy.damage) hearts from \(enemy.name), \(hearts) hearts left.")
+        if hearts == 0 {
+            self.delegate.gameDidEnd(self)
+        }
+    }
+    func heal(item item : Healable) {
+        hearts += item.heartsRestores
+        print("\(name) gained \(item.heartsRestores) hearts from a \(item.healableName), and has \(hearts) hearts left.")
+    }
+}
 
+class GameManager : GameManagerDelegate {
+    func gameDidStart(game: TheLegendOfZelda) {
+        print("It's dangerous to go alone. Take this.")
+        game.sword = "Wooden"
+        game.hearts = 3.0
+    }
+    func gameDidEnd(game: TheLegendOfZelda) {
+        print("GAME OVER. RETURN OF GANON")
+    }
+}
 
-
-
+let link = TheLegendOfZelda(delegate: GameManager())
+let keese = Enemy(name: "Keese", damage: 0.5)
+let dekuBaba = Enemy(name: "Deku Baba", damage: 3.0)
+let ganon = Enemy(name: "Ganon", damage: 2.0)
+link.takeDamage(fromEnemy: keese)
+link.heal(item : BluePotion())
+link.takeDamage(fromEnemy: keese)
+link.takeDamage(fromEnemy: dekuBaba)
+link.heal(item: Heart())
+link.takeDamage(fromEnemy: ganon)
+link.takeDamage(fromEnemy: ganon)
 
 
 
