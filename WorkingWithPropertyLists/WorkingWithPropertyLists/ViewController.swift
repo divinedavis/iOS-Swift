ed56a4a487c3bf0261cb576866443de94412bb13
divinedavis <divinejdavis@gmail.com>
2017-02-06 17:46:57 -0500

    Persisted data with property list

--- WorkingWithPropertyLists/WorkingWithPropertyLists/ViewController.swift
+++ WorkingWithPropertyLists/WorkingWithPropertyLists/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var favColorLabel: UILabel!
+    @IBOutlet weak var favNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.loadFavorites()
+    }
+    
+    func loadFavorites() {
+        
+        if let path = Bundle.main.url(forResource: "favList", withExtension: "plist") {
+            
+            let list = NSDictionary(contentsOf: path)
+            
+            if let _ = list?.object(forKey: "demoDisplayFavAtStart") {
+                
+                if let favNumber = list?.object(forKey: "demoFavNumber") {
+                    favNumberLabel.text = "\(favNumber)"
+                }
+                
+                if let favColor = list?.object(forKey: "demoFavColor") {
+                    favColorLabel.text = "\(favColor)"
+                }
+            }
+            
+            else {
+                print("not displaying favorites")
+            }
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
